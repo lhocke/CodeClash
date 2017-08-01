08d2fcb20bfa975cc659c35cb21105a960badb8a
HQ <Spawn@calvisitor-10-105-169-147.calvisitor.1918.berkeley.edu>
2017-08-01 14:07:58 -0700

    Adding routing and model files

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+var db = require("./models");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // why true?
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+// app.use(express.static("public"));
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
