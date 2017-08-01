be646286b89e31a4471db814d5c6e425fb25d186
HQ <Spawn@calvisitor-10-105-169-147.calvisitor.1918.berkeley.edu>
2017-08-01 14:34:33 -0700

    Cleaning server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var exphbs = require("express-handlebars");
 
 var PORT = process.env.PORT || 7500;
 
@@ -9,48 +10,22 @@ var app = express();
 var db = require("./models");
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 var exphbs = require('express-handlebars');
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require('./routes/control.js');
+require("./routes/api-routes.js")(app);
 
 app.use('/', routes);
 
-db.sequelize.sync({}).then(function() {
-=======
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-
-var db = require("./models");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // why true?
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-// app.use(express.static("public"));
-
-// Set Handlebars.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
 db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
