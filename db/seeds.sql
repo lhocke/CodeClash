0ad608db66a949b080dcef5c06a80c6e43269479
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-08-03 11:38:39 -0700

    adds resize of codemirror

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,297 @@
-INSERT INTO Questions(question_text, question_func, valid_args) VALUES ("this", "that", "those");
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES('Fix This Function', 'function(a,b) {', '    a + b', '}', '2,3;5,6', '5;11');
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES('Write a function for which mutiples of 3 return Fizz, multiples of 5 return Buzz, and multiples of 15 return FizzBuzz', 'function(n) {', '//Your Code Here', '}', '6;20;12;40;45;60', 'Fizz;Buzz;Fizz;Buzz;FizzBuzz;FizzBuzz');
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES('Create a function that that takes a string and returns the midle letters.\nIf there are an odd number of letters return only the middle letter (i.e. James should return "m"). If there are an even number of letters it should return the two midddle letters ("letter" should return "tt").\nGood Luck!', 'function(n) {', '    //Your Code Here', '}', 'letter;John;Roper;Saumya;David;apple;Jeff;Jack;','tt;oh;p;um;v;p;ef;ac');
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
+INSERT INTO Questions(question_text, question_func1, question_func2, question_func3, valid_args, exp_val) 
+VALUES
+
