201159c5d08370001c8a51c52d96d46ada0e8ea2
Saumya <kdevara.saumya@gmail.com>
2017-08-03 22:19:05 -0700

    passport google Oauth

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	
+
+	'googleAuth' :{
+		'clientID': '1066347284888-7earr938s0hfafqepcgi6r4e8be8e850.apps.googleusercontent.com',
+		'clientSecret': 'CEYhULuOnsL2DFVDO0mUkDJn',
+		'callbackURL': 'http://localhost:7500/auth/google/callback'
+	}
+}
--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,18 @@ var passport = require('passport');
 var db = require("../models");
 var LocalStrategy = require('passport-local').Strategy;
 
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// var User = require('./models/user');
+var configAuth = require('./auth');
+
+
 //checking if user exists & if passport correct
 //local login using usernam and passsword to determine 
 // Login form, we named the username field "username" and we didn't have to do any further config
-// because passports looks for that name specifically.  
+// because passports looks for that name specifically. 
+module.exports = function(passport) {
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
     db.users.findOne({where: {username:username}}).then(function (user, err) {
@@ -31,3 +39,37 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
+
+passport.use(new GoogleStrategy({
+      clientID: configAuth.googleAuth.clientID,
+      clientSecret: configAuth.googleAuth.clientSecret,
+      callbackURL: configAuth.googleAuth.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function(){
+          db.user.findOne({'google.id': profile.id}, function(err, user){
+            if(err)
+              return done(err);
+            if(user)
+              return done(null, user);
+            else {
+              var newUser = new User();
+              newUser.google.id = profile.id;
+              newUser.google.token = accessToken;
+              newUser.google.name = profile.displayName;
+              newUser.google.email = profile.emails[0].value;
+
+              newUser.save(function(err){
+                if(err)
+                  throw err;
+                return done(null, newUser);
+              })
+              console.log(profile);
+            }
+          });
+        });
+      }
+
+  ));
+
+};
