289a655522daec77cfab3e04d3c5713e11316fc5
lhocke <dylan.ishihara@gmail.com>
2017-08-03 18:12:06 -0700

    adds routes for static html, previous routes commented out

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,24 +1,29 @@
 var path = require('path');
 
 module.exports = function(app, passport) {
-    app.get('/', function (req, res, next) {
-        res.render('signin', {layout: 'signin-layout'});
-    });
-    
+    // app.get('/', function (req, res, next) {
+    //     res.render('signin', {layout: 'signin-layout'});
+    // });
+    app.get('/', function(req, res, next) {
+        res.sendFile(path.join(__dirname, "../public/signin.html"));
+    })
     app.get('/arena', function(req, res, next) {
         res.sendFile(path.join(__dirname, '../public/mirror.html'));
     });
 
     app.get('/profile', isLoggedIn, function(req, res, next) {
-        res.render('profile', {layout: 'profile-layout'});
+        // res.render('profile', {layout: 'profile-layout'});
+        res.sendFile(path.join(__dirname, "../public/profile.html"));
     });
 
     app.get('/signup', function(req, res, next) {
-        res.render('signup', {layout: 'signup-layout'});
+        // res.render('signup', {layout: 'signup-layout'});
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
 
     app.get('/signin', function(req, res, next) {
-        res.render('signin', {layout: 'signin-layout'});
+        // res.render('signin', {layout: 'signin-layout'});
+        res.sendFile(path.join(__dirname, "../public/signin.html"));
     });
 
     app.get('/logout', function(req, res) {
@@ -26,12 +31,12 @@ module.exports = function(app, passport) {
     });
 
     function isLoggedIn(req, res, next) {
- 
-    if (req.isAuthenticated())
      
-        return next();
+        if (req.isAuthenticated())
          
-    res.redirect('/signin');
+            return next();
+             
+        res.redirect('/signin');
  
-};
+    };
 };
