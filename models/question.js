85b9cbb0b1e66c4387615d0f5396b7cdd81ceb82
lhocke <dylan.ishihara@gmail.com>
2017-08-03 19:43:28 -0700

    better code structure

--- models/question.js
+++ models/question.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var Question = sequelize.define("Question", {
         question_text: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 len: [1]
